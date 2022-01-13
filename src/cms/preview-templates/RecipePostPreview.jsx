eb86bdc97a41312b4d04a866faaa8a13a45c1dc1
Julien Usson <julien@usson.me>
2022-01-13 22:21:21 +0100

    ingredient list bug

---
+++ src/cms/preview-templates/RecipePostPreview.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { RecipePostTemplate } from '../../templates/recipe-post'
+
+const RecipePostPreview = ({ entry, getAsset, widgetFor, widgetsFor }) => {
+  const data = entry.get('data').toJS()
+  return (
+    <RecipePostTemplate
+      content={widgetFor('body')}
+      title={data.title || ''}
+      duration={data.duration || ''}
+      servings={data.servings || 1}
+      ingredients={data.ingredients || []}
+      subRecipes={data.subRecipes || []}
+      image={getAsset(entry.getIn(['data', 'image']))?.url}
+      tags={data.tags || []}
+      source={data.source || ''}
+    />
+  )
+}
+
+export default RecipePostPreview
